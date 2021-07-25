d4be5cc634900f356ef855cd4a9926e7763fc5fa
Peter Luna <lunapeter92@gmail.com>
2021-07-25 15:22:55 -0400

    finished backend api

--- routes/movie.js
+++ routes/movie.js
@@ -12,13 +12,27 @@ async function getMovie(req, res){
     let movie = req.query.movie;
     let location = req.query.searchQuery
     let key = process.env.MOVIE_API_KEY;
-    let movieUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&region=${location}`;
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${location}`;
 
     try{
         let result = await axios.get(movieUrl);
         console.log(result.data.results)
-    }catch{
+        let newArr = result.data.results.map(item => {
+            return new Movie(item);
+        })
+        res.status(200).json(newArr);
+        
+        // console.log(result.data.results.data.overview)
+    }catch(error){
         console.error(error);
+    }
+}
+
+
+class Movie {
+    constructor(obj){
+        this.title = obj.title;
+        this.overview = obj.overview;
     }
 }
 
